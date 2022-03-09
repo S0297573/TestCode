f8361f3ab98ec881de8de0a2be0eb6fe1f280dc7
sh_pr <sh.prashanna@gmail.com>
2022-03-08 22:31:24 -0500

    second

--- src/testcode/WAP_string.java
+++ src/testcode/WAP_string.java
@@ -5,6 +5,24 @@ public class WAP_string {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String Ex = "abbc";
+		for(int i=0;i<=Ex.length();i++) {
+			for(int j=i+1;j<=Ex.length();j++) {
+				System.out.println(Ex.substring(i,j));
+			}
+		}
 	}
 
 }
+
+
+//WAP for Substring? Ex- String = abbc, in output- a
+//ab
+//abb
+//abbc
+//b
+//bb
+//bbc
+//b
+//bc
+//c
